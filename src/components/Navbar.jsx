5ccda7be8dce5a7b9bd3c361ecf0c7db23ff7fe2
amritranjandev <108569150+amritranjandev@users.noreply.github.com>
2025-04-27 18:18:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from '../assets/kevinRushLogo.png'
+// import logo from '../assets/kevinRushLogo.png'
+import logo from '../assets/Ar.png'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 // import { FaSquareXTwitter } from 'react-icons/fa' 
@@ -10,14 +11,24 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo" />
+            {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
+            <span className="mx-2 w-10 font-bold text-xl">Ar</span>
+
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin/>
-            <FaGithub/>
+            <a href="https://www.linkedin.com/in/amrit-ranjan-professional/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin />
+            </a>
+            {/* <FaGithub/> */}
             {/* <FaSquareXTwitter/>  */}
-            <FaInstagram/>
-            <FaMedium/>
+            {/* <FaInstagram/> */}
+            {/* <FaMedium/> */}
+            <a href="https://medium.com/@amritranjanamc" target="_blank" rel="noopener noreferrer">
+                <FaMedium />
+            </a>
+            <a href="https://github.com/amritranjandev" target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+            </a>
         </div>
     </nav>
   )
